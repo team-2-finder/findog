9ff54c6c90e99fd4c29297f3b16fa3a4848bc0ee
Jaeyong Sung <jaeyong0201@gmail.com>
2023-06-30 00:19:38 +0900

    feat: pwa

--- public/src/components/index.js
+++ public/src/components/index.js
@@ -9,6 +9,10 @@ import Label from "./Label";
 import DetailModal from "./DetailModal";
 import DetailModal2 from "./DetailModal2";
 import MBottomNavBar from "./MBottomNavBar";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
+
 export {
   Header,
   MainColor,
