ca51673a1021573118dab77174d0e96962c6d3c1
Jaeyong Sung <jaeyong0201@gmail.com>
2023-06-30 00:21:11 +0900

    fix: pwa

--- public/src/components/index.js
+++ public/src/components/index.js
@@ -9,9 +9,6 @@ import Label from "./Label";
 import DetailModal from "./DetailModal";
 import DetailModal2 from "./DetailModal2";
 import MBottomNavBar from "./MBottomNavBar";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-serviceWorkerRegistration.register();
 
 export {
   Header,
--- public/src/index.js
+++ public/src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+serviceWorkerRegistration.register();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
