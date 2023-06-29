36e7b56056406b04f4054f7d28b58486e4e25032
Jaeyong Sung <jaeyong0201@gmail.com>
2023-06-29 15:29:56 +0900

    feat: add startup

--- ai/server/__init__.py
+++ ai/server/__init__.py
@@ -1,10 +1,12 @@
 from sqlalchemy import select
+from server.utils.preprocess import get_crops
 from server.db import Database, model
 from server.utils.acc import get_hist_acc
 from fastapi import FastAPI
 
 app = FastAPI()
 
+images = []
 
 async def get_all_paths() -> list[model.Dogs]:
     async with Database.async_session() as session:
@@ -22,4 +24,10 @@ async def read_item(path: str):
 
 @app.on_event("startup")
 async def startup():
+    global images
     await Database.init()
+    dogs = await get_all_paths()
+    paths = [dog.image_path for dog in dogs]
+    images = get_crops(paths)
+
+
